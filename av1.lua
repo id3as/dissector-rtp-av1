de67ac08a448dcdce58d152aaeeed4bdb56530a7
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2024-08-14 23:46:04 +0100

    clean up field init

--- av1.lua
+++ av1.lua
@@ -1,34 +1,11 @@
+------------------------------------------------------------------------------------------------
 -- Lua Dissector for AV1 in RTP
---
---
 ------------------------------------------------------------------------------------------------
 do
     local av1 = Proto("av1", "AV1")
 
     local prefs = av1.prefs
     prefs.dyn_pt = Pref.uint("AV1 dynamic payload type", 0, "XXX")
-
-    local F = av1.fields
-
-    F.AGGR = ProtoField.uint8("av1.AggregateHeader", "Aggregate Header", base.HEX)
-
-    F.CONTINUATION = ProtoField.bool("av1.Continuation", "1st OBU is continuation", 8, nil, 0x80,
-        "Does the first OBU continue from the previous packet")
-    F.CONTINUES = ProtoField.bool("av1.Continues", "Last OBU continues", 8, nil, 0x40,
-        "Does the last OBU continue in the next packet")
-    F.NUM_OBUS = ProtoField.uint8("av1.NumObus", "Number of OBUs", base.DEC, nil, 0x30)
-    F.CVS_START = ProtoField.bool("av1.CvsStart", "CVS start", 8, nil, 0x08)
-    F.RESERVED = ProtoField.uint8("av1.Reserved", "Reserved", base.HEX, nil, 0x07)
-
-
-    F.DATA = ProtoField.bytes("av1.PayloadData", "Payload Data")
-
-    F.OBU_LEN = ProtoField.bytes("av1.ObuLen", "OBU length")
-    F.OBU_DATA = ProtoField.bytes("av1.ObuData", "OBU data")
-
-    F.OBU_HEADER = ProtoField.uint8("av1.ObuHeader", "OBU header", base.HEX)
-
-    F.OBU_HEADER_FORBIDDEN = ProtoField.bool("av1.ObuHeader.ForbiddenBit", "Forbidden Bit", 8, nil, 0x80)
 
     local obu_types = {
         [0] = "Reserved",
@@ -43,11 +20,24 @@ do
         [15] = "OBU_PADDING"
     }
 
-    F.OBU_TYPE = ProtoField.uint8("av1.ObuHeader.ObuType", "OBU type", base.HEX, obu_types, 0x78)
-    F.OBU_EXTENSION_FLAG = ProtoField.bool("av1.ObuHeader.ExtensionFlag", "Extension Flag", 8, nil, 0x04)
-    F.OBU_HAS_SIZE_FIELD = ProtoField.bool("av1.ObuHeader.HasSizeField", "Has Size Field", 8, nil, 0x02)
-    F.OBU_RESERVED_1BIT = ProtoField.bool("av1.ObuHeader.ReservedBit", "Reserved 1 Bit", 8, nil, 0x01)
-
+    local f = {
+        aggr = ProtoField.uint8("av1.AggregateHeader", "Aggregate Header", base.HEX),
+        continuation = ProtoField.bool("av1.first_continuation", "1st OBU is continuation", 8, nil, 0x80,        "Does the first OBU continue from the previous packet"),
+        continues = ProtoField.bool("av1.last_continues", "Last OBU continues", 8, nil, 0x40,        "Does the last OBU continue in the next packet"),
+        num_obus = ProtoField.uint8("av1.num_obus", "Number of OBUs", base.DEC, nil, 0x30),
+        cvs_start = ProtoField.bool("av1.cvs_start", "CVS start", 8, nil, 0x08),
+        reserved = ProtoField.uint8("av1.reserved", "Reserved", base.HEX, nil, 0x07),
+        data = ProtoField.bytes("av1.payload_data", "Payload Data"),
+        obu_len = ProtoField.bytes("av1.obu_len", "OBU length"),
+        obu_data = ProtoField.bytes("av1.obu_data", "OBU data"),
+        obu_header = ProtoField.uint8("av1.obu_header", "OBU header", base.HEX),
+        obu_header_forbidden = ProtoField.bool("av1.ObuHeader.ForbiddenBit", "Forbidden Bit", 8, nil, 0x80),
+        obu_type = ProtoField.uint8("av1.obu_header.obu_type", "OBU type", base.HEX, obu_types, 0x78),
+        obu_extension_flag = ProtoField.bool("av1.obu_header.extension_flag", "Extension Flag", 8, nil, 0x04),
+        obu_has_size_field = ProtoField.bool("av1.obu_header.has_size_field", "Has Size Field", 8, nil, 0x02),
+        obu_reserved_1bit = ProtoField.bool("av1.obu_header.reserved_bit", "Reserved 1 Bit", 8, nil, 0x01)
+    }
+    av1.fields = f
 
     local function leb128(range)
         local value = 0
@@ -55,7 +45,6 @@ do
         for i = 0, 8 do
             local leb128_byte = range(i, 1):uint()
             value = bit.bor(value, bit.lshift(bit.band(leb128_byte, 0x7f), (i * 7)))
-            print("Loop i=" .. i .. ", value=" .. value)
             leb128bytes = leb128bytes + 1
             if bit.band(leb128_byte, 0x80) == 0 then
                 break
@@ -66,28 +55,23 @@ do
     end
 
     function av1.dissector(tvb, pinfo, tree)
-        print("HELLO AV1 dissector");
         local subtree = tree:add(av1, tvb(), "AV1 Data")
         local aggr_range = tvb(0, 1)
-        local aggr = subtree:add(F.AGGR, aggr_range)
-        aggr:add(F.CONTINUATION, aggr_range)
-        aggr:add(F.CONTINUES, aggr_range)
-        aggr:add(F.NUM_OBUS, aggr_range)
+        local aggr = subtree:add(f.aggr, aggr_range)
+        aggr:add(f.continuation, aggr_range)
+        aggr:add(f.continues, aggr_range)
+        aggr:add(f.num_obus, aggr_range)
         local is_continuation = aggr_range:bitfield(0, 1)
         local num_obus = aggr_range:bitfield(2, 2)
-        print("num_obus=" .. num_obus)
-        aggr:add(F.CVS_START, aggr_range)
-        aggr:add(F.RESERVED, aggr_range)
-
-        -- subtree:add(F.DATA, tvb(1))
+        aggr:add(f.cvs_start, aggr_range)
+        aggr:add(f.reserved, aggr_range)
 
         local function add_header(header, header_byte)
-            print("adding header: "..header_byte)
-            header:add(F.OBU_HEADER_FORBIDDEN, header_byte)
-            header:add(F.OBU_TYPE, header_byte)
-            header:add(F.OBU_EXTENSION_FLAG, header_byte)
-            header:add(F.OBU_HAS_SIZE_FIELD, header_byte)
-            header:add(F.OBU_RESERVED_1BIT, header_byte)
+            header:add(f.obu_header_forbidden, header_byte)
+            header:add(f.obu_type, header_byte)
+            header:add(f.obu_extension_flag, header_byte)
+            header:add(f.obu_has_size_field, header_byte)
+            header:add(f.obu_reserved_1bit, header_byte)
         end
 
 
@@ -95,7 +79,7 @@ do
         local is_first = true
         while true do
             if num_obus == 1 then
-                local header = subtree:add(F.OBU_DATA, obu_range)
+                local header = subtree:add(f.obu_data, obu_range)
                 if is_continuation ~= 1 then 
                     local header_byte = obu_range(0, 1)
                     add_header(header, header_byte)
@@ -107,9 +91,8 @@ do
                 end
                 local length, bytes = leb128(obu_range)
                 local data = obu_range(bytes, length)
-                print("length=" .. length)
-                subtree:add(F.OBU_LEN, obu_range(0, bytes)):append_text(" (" .. length .. ")")
-                local header = subtree:add(F.OBU_DATA, data)
+                subtree:add(f.obu_len, obu_range(0, bytes)):append_text(" (" .. length .. ")")
+                local header = subtree:add(f.obu_data, data)
                 if not (is_first and is_continuation == 1) then
                     local header_byte = data(0, 1)
                     add_header(header, header_byte)
